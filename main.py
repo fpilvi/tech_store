6f10210f8d4d2377e8185870946180c840dcbfdf
fpilvi <florentine.moehrle@gmail.com>
2024-11-14 12:28:25 +0100

    Added store menu function

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 
+def display_menu():
+    print("Store Menu\n----------")
+    print("1. List all products in store")
+    print("2. Show total amount in store")
+    print("3. Make an order")
+    print("4. Quit")
+    choice = input("Please choose a number: ")
+    return choice
